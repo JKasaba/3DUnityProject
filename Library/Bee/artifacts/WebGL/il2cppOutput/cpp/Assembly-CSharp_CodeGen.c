1ec1418ddd66a10ef6046020381d12abac9f0204
JKasaba <james@kasaba.org>
2024-02-16 00:33:14 -0800

    initial commit

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void Helloworld_Start_mCA4368C9B683F40EA515431B9194E088D859F897 (void);
+extern void Helloworld_Update_m028824878C9616C4C44C58E2179C2CD8EF63F2DD (void);
+extern void Helloworld__ctor_m22C4474DBA0FA9A508898B5F410DD175B9461FE6 (void);
+extern void RollingBall_Start_mF217FE69ECB890601502956653EC5653738D67DC (void);
+extern void RollingBall_Update_m33DB885790A4D42685CEFA06E0931D3A9A0DF254 (void);
+extern void RollingBall__ctor_m7119AC6DAA41D14533C078CC25582B7C96071C6F (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[8] = 
+{
+	Helloworld_Start_mCA4368C9B683F40EA515431B9194E088D859F897,
+	Helloworld_Update_m028824878C9616C4C44C58E2179C2CD8EF63F2DD,
+	Helloworld__ctor_m22C4474DBA0FA9A508898B5F410DD175B9461FE6,
+	RollingBall_Start_mF217FE69ECB890601502956653EC5653738D67DC,
+	RollingBall_Update_m33DB885790A4D42685CEFA06E0931D3A9A0DF254,
+	RollingBall__ctor_m7119AC6DAA41D14533C078CC25582B7C96071C6F,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[8] = 
+{
+	1098,
+	1098,
+	1098,
+	1098,
+	1098,
+	1098,
+	2121,
+	1098,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	8,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
